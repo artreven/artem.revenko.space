332ed682648c6b1b071281eb461ebb19146b29d7
Artem Revenko <artreven@gmail.com>
2016-06-19 03:39:15 +0200

    Correct wsgi app name

--- artem_revenko_space/wsgi.py
+++ artem_revenko_space/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artem_revenko_sapce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artem_revenko_space.settings")
 
 application = get_wsgi_application()
